f30e7643578127371d8eea935c4e881adf30bf7c
seanmcquaid <sean.mcquaid2@verizon.net>
2020-04-12 22:55:56 -0400

    only tests left involve global context

--- src/navigation/AppNavigator.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AppNavigator from "./AppNavigator";
-
-describe("<AppNavigator/>", () => {
-    it("App Navigator returns the correct screen", () => {
-
-    });
-});
